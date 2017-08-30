75fa0a55f24e4be33c9a69b594efddcbeedffa99
renjitpeter <mailrenjit@gmail.com>
2017-08-30 13:38:36 +0530

    Added project a created the initial view controller skelton

---
+++ MovieAppForMS/NowPlayingViewController.swift
@@ -0,0 +1,19 @@
+//
+//  NowPlayingViewController.swift
+//  MovieAppForMS
+//
+//  Created by Renjit Peter on 8/30/17.
+//  Copyright © 2017 Renjit Peter. All rights reserved.
+//
+
+import UIKit
+
+private let reuseIdentifier = "Cell"
+
+class NowPlayingViewController: UICollectionViewController {
+
+    //MARK: - properties
+    fileprivate let reuseidentifier = "MovieCell"
+    fileprivate let sectionInsets = UIEdgeInsets(top: 20.0, left: 20.0, bottom: 20.0, right: 20.0)
+
+}
